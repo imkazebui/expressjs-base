568160eaa43f3aae891bdd281c38cbf2c8a992f9
phuong.bui <phuong@STS-PHUONGBUI.local>
2021-04-28 14:53:13 +0700

    update jwt

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import path from "path";
 import logger from "morgan";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
+const passport = require("passport");
 
 import { errorHandler } from "./middleware/error.js";
 import { notFoundHandler } from "./middleware/not-found.js";
+
+import configPassport from "./config/passport";
 
 const __dirname = path.resolve(path.dirname(""));
 
 const app = express();
 const port = 3000;
+
+configPassport(passport);
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -19,6 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(passport.initialize());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
